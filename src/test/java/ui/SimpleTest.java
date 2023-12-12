6aac4ec55522625235ab3f189bc56fc6a1b58597
Max <mkatr@softserveinc.com>
2023-12-12 12:42:22 +0200

    init commit

---
+++ src/test/java/ui/SimpleTest.java
@@ -0,0 +1,32 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    private WebDriver driver;
+
+    //only if one thread
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//a[@href='/checkboxes']")).click();
+        driver.findElements(By.xpath("//input")).forEach(WebElement::click);
+    }
+
+    @AfterClass
+    public void finish() {
+        driver.close();
+    }
+}
