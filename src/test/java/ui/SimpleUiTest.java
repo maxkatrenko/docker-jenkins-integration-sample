613b1e227bd5bee9320bfe99ba96b497f82bcbdf
Max <mkatr@softserveinc.com>
2023-12-12 13:12:21 +0200

    second commit

--- src/test/java/ui/SimpleUiTest.java
+++ src/test/java/ui/SimpleUiTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SimpleTest {
+public class SimpleUiTest {
 
     private WebDriver driver;
 
